26eec0db3a71709d4820d8157bb58c72a052a92c
nkc23chanchinlung <134259379+nkc23chanchinlung@users.noreply.github.com>
2026-03-02 14:57:37 +0900

    ボス部屋透明化した

---
+++ Assets/Script/Wall.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+   [SerializeField] Transform _playerPos;
+    Material _wallMat;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         _wallMat =gameObject. GetComponent<Renderer>().material;
+        _playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float alpha;
+        float distance = Vector3.Distance(transform.position, _playerPos.position);
+        Debug.Log(distance);
+        alpha = Mathf.Clamp(distance, 125, 255); // 距離に応じて透明度を計算
+        if (distance <= 15) alpha-=0.5f;
+        else alpha+=0.5f;
+            _wallMat.color = new Color(1, 1, 1, alpha / 255f); // プレイヤーからの距離に応じて壁の透明度を変える
+    }
+}
